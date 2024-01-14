467688be8105210044334db14a84f5929fd25507
Aimar Mundiñano Jimenez <aimar.mundinano@codehouse.academy>
2024-01-14 11:40:00 +0100

    Corrección fornato app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -42,30 +42,31 @@ export class AppComponent {
       });
       this.startToasts = [...this.config.getStartMsgs()];
     }
-  printWelcome() {
-    console.log('%cBienvenid@ a Agraja!','font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)');
-    console.log('%cCreada con ❤️ por Aimarmun', 'color: green; font-family:monospace;');
-    console.log(`Gracias por probar Agraja!
-      Agraja es mi proyecto final de Bootcamp realizado para Codehouse Academy (https://codehouse.academy/).
-      Se trata de una aplicación que tiene dos apartados bien difenciados:
-        -Agro: para la contratación de agricultores.
-        -Caja: para la venta de cajas de productos.
-      Las cajas pueden ser de frutas, cereales, leguminosas u hortalizas.
-      Los agricultores pueden tratar 4 tipos de cultivos: hortalizas, leguminosas, frutales y cereales.
-      Agraja contiene una lista de clientes. Estos clientes pueden contratar los servicios de un agricultor o pueden realizar compras de cajas.
-      
-      Agraja tiene disponible 2 usuarios diferentes, el usuario Administrador y el usuario Vendedor.
-      El usuario Administrador permite hacer contrataciones de agricultores y ventas de cajas, pero además permite modificar y añadir tanto cajas como agricultores.
-      
-      El usuario Vendedor permite realizar contrataciones y vender cajas, pero no permite la modificación de estos.
-      
-      ⚠️ Ten en cuenta que la base de datos se reinicia cada hora y si coincides con este proceso, es posible que te saque de la sesión.
-            
-      Para la autenticación se ha utilizado Jwt Bearer tokens y las contraseñas se cifran mediante AES256 en la base de datos.
-      Solo se permite el inicio de sesión de un mismo usuario a la vez, por lo que es posible que si alguien está utilizando el mismo usuario desde otro navegador te tire de la sesión.
-      
-      Muy pronto publicaré el codigo fuente en Github.`);
-  }
+    
+    printWelcome() {
+      console.log('%cBienvenid@ a Agraja!','font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)');
+      console.log('%cCreada con ❤️ por Aimarmun', 'color: green; font-family:monospace;');
+      console.log(`Gracias por probar Agraja!
+        Agraja es mi proyecto final de Bootcamp realizado para Codehouse Academy (https://codehouse.academy/).
+        Se trata de una aplicación que tiene dos apartados bien difenciados:
+          -Agro: para la contratación de agricultores.
+          -Caja: para la venta de cajas de productos.
+        Las cajas pueden ser de frutas, cereales, leguminosas u hortalizas.
+        Los agricultores pueden tratar 4 tipos de cultivos: hortalizas, leguminosas, frutales y cereales.
+        Agraja contiene una lista de clientes. Estos clientes pueden contratar los servicios de un agricultor o pueden realizar compras de cajas.
+        
+        Agraja tiene disponible 2 usuarios diferentes, el usuario Administrador y el usuario Vendedor.
+        El usuario Administrador permite hacer contrataciones de agricultores y ventas de cajas, pero además permite modificar y añadir tanto cajas como agricultores.
+        
+        El usuario Vendedor permite realizar contrataciones y vender cajas, pero no permite la modificación de estos.
+        
+        ⚠️ Ten en cuenta que la base de datos se reinicia cada hora y si coincides con este proceso, es posible que te saque de la sesión.
+              
+        Para la autenticación se ha utilizado Jwt Bearer tokens y las contraseñas se cifran mediante AES256 en la base de datos.
+        Solo se permite el inicio de sesión de un mismo usuario a la vez, por lo que es posible que si alguien está utilizando el mismo usuario desde otro navegador te tire de la sesión.
+        
+        Muy pronto publicaré el codigo fuente en Github.`);
+    }
 
     isLogged(): boolean{
       return this.authService.isLogged();
